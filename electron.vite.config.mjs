8ddf147127b104e80ea36656827a7d7e47199857
Hoshu <fbenjamin.africano@gmail.com>
2025-12-10 00:04:13 +0800

    Fix build scripts for Vercel

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
